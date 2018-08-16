6d3db84fb630d88b3923ff25e4eb5be3c3b9ef8f
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-08-16 16:54:25 +1000

    Basic Movement + Define

--- src/main.ino
+++ src/main.ino
@@ -7,7 +7,7 @@
 //Camera camera;
 //MotorController Motor;
 
-//All the variable naming goes
+//All the variable naming goes here
 
 void setup() {
   Serial.begin(9600);
@@ -20,5 +20,17 @@ void loop() {
   //camera.update();
   Serial.print(Serial3.read());
   //Motor.move(0, 0, 150);
+
+  //Movement If Statements
+
+  if (camera.ballAngle > 340 || camera.ballAngle < 20) {
+    bAngle = 0
+  } else {
+    if (camera.ballAngle > 180) {
+      bAngle = camera.ballAngle - 90;
+    } else {
+        bAngle = camera.ballAngle + 90;
+        }
+      }
 
 }
