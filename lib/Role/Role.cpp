0bdfbca1c7833c35b22c0b03269fa8f11944ba56
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-09-24 11:52:17 +1000

    Defender Update

--- lib/Role/Role.cpp
+++ lib/Role/Role.cpp
@@ -25,11 +25,11 @@ void Role::Defend(int mvspeed, int dir, int angle, int balldis) {
   if (angle <= 15 || angle >= 345) {
     Motor.Move(0, 0, 50);
   } else if (angle >= 270) {
-    goalmove = (angle - 2 * (angle - 180)*1.5 + 120);
+    goalmove = (-0.0104938 * pow(angle, 2) + 3.77778 * angle + (-1.1369 * pow(10, -14)));
     //Remember to fix this shit lmao
     Motor.Move(270, 0, goalmove);
   } else if (angle <= 90) {
-    goalmove = (angle*1.5) + 120;
+    goalmove = (-0.0104938 * pow(angle, 2) + 3.77778 * angle + (-1.1369 * pow(10, -14)));
     Motor.Move(90, 0, goalmove);
   } else {
     action(255, 0, dir, -1, camera.ballDistance);
