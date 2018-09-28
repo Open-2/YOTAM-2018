e28cfb3ca6e994e4461a435df07ee5734f78ee6e
Godliketitan <32060861+Godliketitan@users.noreply.github.com>
2018-09-28 11:58:51 +1000

    Camera Stuff

--- lib/Camera Write/YOTAM Camera Detection.py
+++ lib/Camera Write/YOTAM Camera Detection.py
@@ -6,10 +6,10 @@ from pyb import UART, LED
 robot = 1
 
 #DEBUGGING & TOGGLES (Set to false before Competitions)
-draw_cross = False      #Draws centre cross
+draw_cross = True      #Draws centre cross
 draw_rect = True       #Draws rectangle around blobs
-draw_line = False       #Draws line from centre cross to centre of blobs
-xy_coords = True       #Print the X & Y coords of the Blobs
+draw_line = True       #Draws line from centre cross to centre of blobs
+xy_coords = False       #Print the X & Y coords of the Blobs
 led_flash = False       #Flashes LED quickly
 print_out = False       #Prints the output values
 fake_output = False     #Fakes output values
@@ -20,15 +20,15 @@ angle_print = False     #Prints the ball angle
 master_debug = False  #If true it turns off all the debug options
 
 if master_debug:
-    draw_cross = False
-    draw_rect = False
-    draw_line = False
-    xy_coords = False
-    led_flash = False
-    print_out = False
-    fake_output = False
-    bdistcent = False
-    angle_print = False
+  draw_cross = False
+  draw_rect = False
+  draw_line = False
+  xy_coords = False
+  led_flash = False
+  print_out = False
+  fake_output = False
+  bdistcent = False
+  angle_print = False
 
 
 
@@ -56,17 +56,21 @@ uart = UART(3, 9600, timeout_char = 1000)
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time = 2000)
+sensor.skip_frames(time = 1000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
 sensor.set_windowing((40,0,240,240))
+#sensor.set_windowing((60,20,200,200))
+
+sensor.set_saturation(3)
+sensor.set_brightness(-2)
+sensor.set_contrast(3)
 
 LED(1).on()
 time.sleep(100)
 LED(1).off()
-sensor.set_saturation(3)
-sensor.set_brightness(-2)
-sensor.set_contrast(0)
+
+
 
 
 #SCANS FOR BIGGEST BLOB
@@ -82,15 +86,13 @@ def BiggestBlob(bBlob):
 
 while(True):
 
-
-
     outBuffer = [255,0,0,0,0,0,0]
 
     img = sensor.snapshot()
 
-    ballBlob = BiggestBlob(img.find_blobs(ball))
-    yellowBlob = BiggestBlob(img.find_blobs(yellowGoal,x_stride=6,y_stride=4,merge=True,margin=34,area_threshold=15))
-    blueBlob = BiggestBlob(img.find_blobs(blueGoal,x_stride=6,y_stride=4,merge=True,margin=34,area_threshold=15))
+    ballBlob = BiggestBlob(img.find_blobs(ball,x_stride=4,y_stride=4,pixels_threshold=20))
+    yellowBlob = BiggestBlob(img.find_blobs(yellowGoal,x_stride=15,y_stride=8,merge=True,margin=34,pixels_threshold=100))
+    blueBlob = BiggestBlob(img.find_blobs(blueGoal,x_stride=15,y_stride=8,merge=True,margin=34,pixels_threshold=50))
 
     #Fake Outputs Debugging
     if fake_output:
@@ -124,46 +126,51 @@ while(True):
 ##Debugging & Drawing
 
         #Draw Rectangle
-        if ballBlob != None and draw_rect:
-            img.draw_rectangle(ballBlob.rect())
-            img.draw_cross(ballBlob.cx(), ballBlob.cy())
-            img.draw_string(ballBlob.cx(), ballBlob.cy(), str(ballBlob.code()),
-                             color=(255, 0, 0))
+        if draw_rect:
+            if ballBlob != None:
+                img.draw_rectangle(ballBlob.rect(),color=(255,92,0))
+                img.draw_cross(ballBlob.cx(), ballBlob.cy())
+                img.draw_string(ballBlob.cx(), ballBlob.cy(), str(ballBlob.code()),
+                                 color=(255, 0, 0))
 
-        if yellowBlob != None and draw_rect:
-            img.draw_rectangle(yellowBlob.rect())
-            img.draw_cross(yellowBlob.cx(), yellowBlob.cy())
-            img.draw_string(yellowBlob.cx(), yellowBlob.cy(), str(yellowBlob.code()),
-                             color=(255, 0, 0))
+            if yellowBlob != None:
+                img.draw_rectangle(yellowBlob.rect(),color=(255,255,0))
+                img.draw_cross(yellowBlob.cx(), yellowBlob.cy())
+                img.draw_string(yellowBlob.cx(), yellowBlob.cy(), str(yellowBlob.code()),
+                                 color=(255, 0, 0))
 
-        if blueBlob != None and draw_rect:
-            img.draw_rectangle(blueBlob.rect())
-            img.draw_cross(blueBlob.cx(), blueBlob.cy())
-            img.draw_string(blueBlob.cx(), blueBlob.cy(), str(blueBlob.code()),
-                             color=(255, 0, 0))
+            if blueBlob != None:
+                img.draw_rectangle(blueBlob.rect(),color=(0,75,255))
+                img.draw_cross(blueBlob.cx(), blueBlob.cy())
+                img.draw_string(blueBlob.cx(), blueBlob.cy(), str(blueBlob.code()),
+                                 color=(255, 0, 0))
 
 
 
         #Draw Line
-        if ballBlob != None and draw_line:
-            img.draw_line((centreX, centreY, ballBlob.cx(), ballBlob.cy()))
+        if draw_line:
+            if ballBlob != None:
+                img.draw_line((centreX, centreY, outBuffer[1], outBuffer[2]),
+                        color=(255,92,0),thickness=2)
+            if yellowBlob != None:
+                img.draw_line((centreX, centreY, outBuffer[3], outBuffer[4]),
+                        color=(255,255,0),thickness=2)
+            if blueBlob != None:
+                img.draw_line((centreX, centreY, outBuffer[5], outBuffer[6]),
+                        color=(0,75,255),thickness=2)
 
-        if yellowBlob != None and draw_line:
-            img.draw_line((centreX, centreY, yellowBlob.cx(), yellowBlob.cy()))
-
-        if blueBlob != None and draw_line:
-            img.draw_line((centreX, centreY, blueBlob.cx(), blueBlob.cy()))
 
 
         #Print X and Y Coords
-        if ballBlob != None and xy_coords:
-            print('Ball', ballBlob.cx(), ballBlob.cy())
+        if xy_coords:
+            if ballBlob != None:
+                print('Ball', ballBlob.cx(), ballBlob.cy())
 
-        if yellowBlob != None and xy_coords:
-            print('Yellow', yellowBlob.cx(), yellowBlob.cy())
+            if yellowBlob != None:
+                print('Yellow', yellowBlob.cx(), yellowBlob.cy())
 
-        if blueBlob != None and xy_coords:
-            print('Blue', blueBlob.cx(), blueBlob.cy())
+            if blueBlob != None:
+                print('Blue', blueBlob.cx(), blueBlob.cy())
 
 
         #Draws Cross in Centre of Screen
