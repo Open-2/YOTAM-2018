b8bc56acea4b9ace94add42681ca7f32fa927cec
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-10-04 22:38:14 +1000

    Done

--- lib/Role/Role.h
+++ lib/Role/Role.h
@@ -12,7 +12,10 @@ class Role{
   public:
     void action(int mvspeed, int state, int fakeangle, int balldis, int dir, int comp, int bangle, int correction);
     int mvspeed = 0;
+<<<<<<< HEAD
     void shit(int ygoalx, int ygoaly, int bgoalx, int bgoaly, int ballx, int bally, int dir, int millistart, int correction, int ballAngle, int bAngle, int bgoalCorrect, int ygoalCorrect);
+=======
+>>>>>>> parent of 554f8ae... Updates
 
   private:
     void Defend(int mvspeed, int angle, int balldis, int dir);
